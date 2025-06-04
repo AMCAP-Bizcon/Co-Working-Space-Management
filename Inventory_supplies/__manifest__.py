a442dd255b4301a7ab1e97442b1cbe72e630d463
makisha S <makisha722004@gmail.com>
2025-06-04 18:47:31 +0530

    created a manifest file for Inventory_Supplies

---
+++ Inventory_supplies/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Inventory Supplies',
+    'version': '1.0',
+    'summary': ' ',
+    'sequence': 10,
+    'description': """
+    """,
+    'category': 'Co working space',
+    'website': 'https://www.mkce.ac.in',
+    'depends': ['base'],
+    'data': [
+        ],
+    'demo':[
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
