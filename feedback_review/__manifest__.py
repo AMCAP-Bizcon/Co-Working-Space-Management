0fed4870b7cf2a486be4f72c8eea90aa9098b65a
sowmiya <sowmiyakathiravan2004@gmail.com>
2025-06-04 12:29:49 +0530

    created manifest file for feedback_review

---
+++ feedback_review/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Feedback Review',
+    'version': '1.0',
+    'summary': ' ',
+    'sequence': 10,
+    'description': """
+    """,
+    'category': 'co working space',
+    'website': 'https://www.mkce.ac.in',
+    'depends': ['base'],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+
+    'license': 'LGPL-3',
+}
