2c2e92bb2b64d2ebfe7b60d2348a498cbfba7b8e
Dhanalakshmi <rmagalakshmi798@gmail.com>
2025-06-04 23:12:49 +0530

    created manifest file for event_scheduler

---
+++ event_scheduler/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Event Scheduler',
+    'version': '1.0',
+    'summary': ' ',
+    'sequence': 10,
+    'description': """
+    """,
+    'category': 'co working space',
+    'website': 'https://www.skpec.in',
+    'depends': ['base'],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+
+    'license': 'LGPL-3',
+}
