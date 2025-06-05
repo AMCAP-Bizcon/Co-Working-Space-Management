cefc92016220112df66c44577b6c9c0980a6d944
swathi G <927621bit113@mkce.ac.in>
2025-06-05 11:17:42 +0530

    created my member directory

---
+++ member_directory/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Manage member directory',
+    'version': '1.0',
+    'summary': ' ',
+    'sequence': 10,
+    'description': """
+    """,
+    'category': 'Co working space',
+    'website': 'https://www.mkce.ac.in',
+    'depends': ['base'],
+    'data': [
+        ],
+    'demo':[
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
