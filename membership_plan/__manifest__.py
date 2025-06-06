36649bec18f2befd998bd0b650fe472fbe90c7b3
swathi G <927621bit113@mkce.ac.in>
2025-06-06 11:32:02 +0530

    created manifest file for membership_plan

---
+++ membership_plan/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Manage membership plan',
+    'version': '1.0',
+    'summary': ' ',
+    'sequence': 10,
+    'description': """""",
+    'category': 'Co working space',
+    'website': 'https://www.mkce.ac.in',
+    'depends': ['base'],
+    'data': [
+        ],
+    'demo':[
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
+
